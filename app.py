65d8934455eaed5c929ac8bbf783693c02364254
yuce-furkan <furkanyce95@gmail.com>
2026-02-26 14:34:39 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+def poliçe_ozetle(poliçe_metni):
+    print("Poliçe analiz ediliyor...")
+    # Şimdilik taslak bir özet dönelim
+    return "Analiz Sonucu: Bu bir kasko poliçesidir. İMM limitiniz yüksektir."
+
+test_metni = "Bu bir örnek poliçe metnidir."
+print(poliçe_ozetle(test_metni))
